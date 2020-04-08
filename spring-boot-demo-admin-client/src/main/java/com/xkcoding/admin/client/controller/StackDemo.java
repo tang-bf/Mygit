2190924f8227d882ea493ae02b7a668167e95866
tang-bf <1535710733@qq.com>
2020-04-08 14:50:54 +0800

    springbootadmin

---
+++ spring-boot-demo-admin-client/src/main/java/com/xkcoding/admin/client/controller/StackDemo.java
@@ -0,0 +1,26 @@
+package com.xkcoding.admin.client.controller;
+
+public class StackDemo {
+    double d1= 20.00;
+    static {
+        i=5;
+       // System.out.println(i);
+    }
+    static  int i =1;
+    {
+        k=90;
+       // k++;
+    }
+    int k=2;
+    public static void main(String[] args) {
+        System.out.println("i>>>"+i);
+    }
+    public  void refesh(){
+       for(int i=0;i<2;i++){
+           int b = 6;
+           System.out.println(">>>>>>");
+       }
+        String str = "oo";
+        String str2;
+    }
+}
